559be56dd0e8150ae11d0d4b2da07b249026d410
nelson <nelstaj@yahoo.co.uk>
2020-08-27 18:15:36 +0100

    added checkout page, checkout item component and update cart to use selectors

--- src/redux/cart/dist/cart.selectors.dev.js
+++ src/redux/cart/dist/cart.selectors.dev.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.selectCartItemsCount = exports.selectCartItems = void 0;
+exports.selectCartTotal = exports.selectCartItemsCount = exports.selectCartHidden = exports.selectCartItems = void 0;
 
 var _reselect = require("reselect");
 
@@ -15,9 +15,19 @@ var selectCartItems = (0, _reselect.createSelector)([selectCart], function (cart
   return cart.cartItems;
 });
 exports.selectCartItems = selectCartItems;
+var selectCartHidden = (0, _reselect.createSelector)([selectCart], function (cart) {
+  return cart.hidden;
+});
+exports.selectCartHidden = selectCartHidden;
 var selectCartItemsCount = (0, _reselect.createSelector)([selectCartItems], function (cartItems) {
   return cartItems.reduce(function (accumlatedQuantity, cartItem) {
     return accumlatedQuantity + cartItem.quantity;
   }, 0);
 });
 exports.selectCartItemsCount = selectCartItemsCount;
+var selectCartTotal = (0, _reselect.createSelector)([selectCartItems], function (cartItems) {
+  return cartItems.reduce(function (accumlatedQuantity, cartItem) {
+    return accumlatedQuantity + cartItem.quantity * cartItem.price;
+  }, 0);
+});
+exports.selectCartTotal = selectCartTotal;
